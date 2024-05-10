3e0d992d1798c5e46f55f6ece631fa5915d658e8
Roberto <roberto.menesantos@gmail.com>
2024-05-10 20:55:34 -0300

    feat: creating the project

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import { Text, View, SafeAreaView } from "react-native";
+
+const App = () => {
+    return (
+        <SafeAreaView className="flex-1 justify-center items-center">
+            <Text className="text-3xl">Grupo MAI!</Text>
+        </SafeAreaView>
+    );
+};
+
+export default App;
